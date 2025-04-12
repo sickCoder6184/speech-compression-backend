56e1d94278f366ca076d7829f7288afed44d682e
Preyanshu <preyanshudhapola6184@gmail.com>
2025-04-12 15:24:38 +0530

    fix: added netlify domain to CORS

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://sickcoder6184.github.io",
-        "https://speech-compression.netlify.app"  # if you use Netlify too
+        "https://speech-compression.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
